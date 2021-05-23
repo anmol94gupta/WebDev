13deaf9d55789a996cef90ffdfd521a4147f3ab6
Anmol gupta <gupta.anmol103@gmail.com>
2021-05-23 15:15:42 +0530

    Update todo.test.js
    adding more parameters to the test

--- React/myapp/src/components/__tests__/todo.test.js
+++ React/myapp/src/components/__tests__/todo.test.js
@@ -11,7 +11,9 @@ test('should render non-completed todo ', () => {
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
     expect(todoElement).toHaveTextContent('wash dishes');
-})
+    expect(todoElement).not.toContainHTML('<strike>');
+
+});
 
 test('should render completed todo ', () => {
     const todo = { id: 2, title: 'wash car', completed: true, };
@@ -19,4 +21,5 @@ test('should render completed todo ', () => {
     const todoElement = screen.getByTestId('todo-2');
     expect(todoElement).toBeInTheDocument();
     expect(todoElement).toHaveTextContent('wash car');
-})
+    // expect(todoElement).toContainHTML('<strike>');
+});
